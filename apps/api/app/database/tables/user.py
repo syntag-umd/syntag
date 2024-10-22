a6048e32860d50912af9447cd8dbae298927a6f5
Chipotle0914 <kantitus33@gmail.com>
2024-10-22 16:51:06 -0400

    adding a column for is_admin to the user database

--- apps/api/app/database/tables/user.py
+++ apps/api/app/database/tables/user.py
@@ -2,7 +2,7 @@ from dataclasses import dataclass
 from datetime import datetime
 from typing import Optional
 
-from sqlalchemy import UUID, Column, Integer, Text, text, Float
+from sqlalchemy import UUID, Column, Integer, Text, text, Float, Boolean
 from sqlalchemy.dialects.postgresql import TIMESTAMP
 from sqlalchemy.orm import relationship
 
@@ -45,12 +45,13 @@ class User(Base):
     phone_number_balance: int = Column(
         Integer, nullable=False, server_default=text("0")
     )
+    is_admin_account: bool = Column(Boolean, nullable=False, server_default=text("false"))
     account_balance: float = Column(Float, nullable=False, server_default=text("0"))
 
     account_balance_recharge_threshold: Optional[float] = Column(Float, nullable=True)
     account_balance_recharge_to: Optional[float] = Column(Float, nullable=True)
     account_balance_payment_method: Optional[str] = Column(Text, nullable=True)
-
+    
     conversations = relationship("Conversation", back_populates="user")
     messages = relationship("Message", back_populates="user")
     voice_assistants = relationship("VoiceAssistant", back_populates="user")
