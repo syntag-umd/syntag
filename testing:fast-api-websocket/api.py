a399d592194b1534da6d10effee4cbd9b24fc0f1
Aprameya Kannan <101409453+aprameyak@users.noreply.github.com>
2024-10-26 00:30:48 -0400

    Add files via upload

---
+++ testing:fast-api-websocket/api.py
@@ -0,0 +1,11 @@
+import asyncio
+from fastapi import FastAPI, WebSocket
+
+app = FastAPI()
+
+@app.websocket("/ws")
+async def websocket_endpoint(websocket: WebSocket):
+    await websocket.accept()
+    while True:
+        data = await websocket.receive_text()
+        await websocket.send_text(f"Received: {data}")
