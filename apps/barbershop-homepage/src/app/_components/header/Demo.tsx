5a568edbbe4549f2a79acd4bf4426f48a1c857e3
Aprameya Kannan <101409453+aprameyak@users.noreply.github.com>
2024-11-10 16:55:17 -0500

    Update Demo.tsx

--- apps/barbershop-homepage/src/app/_components/header/Demo.tsx
+++ apps/barbershop-homepage/src/app/_components/header/Demo.tsx
@@ -14,36 +14,19 @@ export default function Demo({ className }: Demo) {
 
   return (
     <div
-      className={`w-72 h-fit bg-[color:var(--card)] text-white max-w-fit p-3 rounded-lg text-center ${className}`}
+      className={`w-full h-full bg-[color:var(--card)] text-white rounded-lg text-center ${className}`}
     >
+       <Image
+          onClick={() => toggleCall()}
+          src="/receptionist.png"
+          alt={"AI Receptionist profile picture"}
+          width={1000}
+          height={1000}
+        />
       {/* <div className="flex flex-row justify-between font-bold text-nowrap text-sm">
                 <div>Try a demo below</div>
                 <div>(555)-555-5555</div>
             </div> */}
-      <div className="flex flex-col items-center gap-2">
-        <Image
-          className="rounded-full"
-          src="/headshot.png"
-          alt={"AI Receptionist profile picture"}
-          width={100}
-          height={100}
-        />
-        <div>
-          {" "}
-          <span className="font-bold"> AI Receptionist </span> is ready to call{" "}
-        </div>
-        <div> Click call to start talking </div>
-        <button
-          className={`${callStatus == CALL_STATUS.ACTIVE ? "bg-red-500" : "bg-[color:var(--primary)]"} w-full m-1 p-1 rounded-md`}
-          onClick={() => toggleCall()}
-        >
-          {callStatus == CALL_STATUS.ACTIVE
-            ? "End Call"
-            : callStatus == CALL_STATUS.LOADING
-              ? "Connecting..."
-              : "Start Call"}
-        </button>
-      </div>
     </div>
   );
 }
